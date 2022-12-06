658bd5745bb102eaab2a465abaa5ce0751dac556
Jorge Limas <jlimas@novalys.local>
2022-12-07 01:18:58 +0300

    Create a base file for solutions

---
+++ base.py
@@ -0,0 +1,6 @@
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, "input.txt"), "r") as input:
+    for line in input:
+        pass
