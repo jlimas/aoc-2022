f479754ed618e5ea588c78b5b15a42f578b3ae37
Jorge Limas <novalys@gmail.com>
2022-12-09 17:30:47 +0300

    Refactor base solution

--- base.py
+++ base.py
@@ -1,6 +1,19 @@
 import os
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-with open(os.path.join(__location__, "input.txt"), "r") as input:
-    for line in input:
-        pass
+
+def parse_file_input(filename):
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+
+    file_data = []
+    with open(os.path.join(__location__, filename), "r") as file_input:
+        for line in file_input:
+            file_data.append(line)
+
+    return file_data
+
+
+if __name__ == "__main__":
+    data = parse_file_input("input.txt")
+    print(data)
